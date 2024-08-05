edd7b1746cdaef4f6780c3415d6f9b52139e3327
Jay <jay@gmail.com>
2024-08-05 13:48:44 -0400

    Day 1 Session 1

---
+++ Readme.md
@@ -0,0 +1,11 @@
+Day 1
+
+Frameworks vs library
+Django
+MVT
+venv/conda
+conda commands  -> venv -> try
+startproject
+settings.py and manage.py
+startapp 
+
---
+++ mysite/login_system_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LoginSystemAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'login_system_app'
