598521ff0aa0e970b9098e00e65da763ef029d81
Jay <jay@gmail.com>
2024-08-06 11:43:09 -0400

    Day 2 Session 1

--- mysite/login_system_app/admin.py
+++ mysite/login_system_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User_data_model
 
 # Register your models here.
+
+@admin.register(User_data_model)
+class User_data(admin.ModelAdmin):
+    list_display = ['id','username', 'age', 'mobile_number', 'email']
--- mysite/login_system_app/models.py
+++ mysite/login_system_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User_data_model(models.Model):
+    username= models.CharField(max_length=255)
+    age= models.IntegerField()
+    # int-> more than digits -> out of range of int
+    mobile_number= models.CharField(max_length=10)
+    email= models.EmailField()
